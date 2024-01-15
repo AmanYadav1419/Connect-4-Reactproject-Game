9acb07c0d9b331b29ecd3e89516bc2d41a43695a
unknown <amanyadav16544@gmail.com>
2024-01-15 03:07:15 -0800

    updating the player circle

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -2,67 +2,43 @@ import React, { useState } from 'react'
 import GameCircle from './GameCircle'
 import '../Game.css'
 
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+
 const GameBoard = () => {
 
   const [gameBoard, setgameBoard] = useState(Array(16).fill(0));
+  const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1)
  
   const circleClicked = (id) => {
     console.log("circle clicked:"+id);
 
-    gameBoard[id] = 1;
-    setgameBoard(gameBoard);
+    const board = [...gameBoard]
+
+    board[id] = currentPlayer;
+    setgameBoard(board);
+
+    setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+    console.log(gameBoard);
+    console.log(currentPlayer);
+  }
+
+  const renderCircle = id =>{
+    return <GameCircle id={id} className={`player_${gameBoard[id]}`} onCircleClicked={circleClicked}/>
   }
 
   return (
     <div className='gameboard'>
-        <GameCircle id={1} className="player_1" onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={2} className="player_2" onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={3} onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={4} onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={5} onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={6} onCircleClicked={circleClicked}>
-
-        </GameCircle>
-        <GameCircle id={7}>
-
-        </GameCircle>
-        <GameCircle id={8}>
-
-        </GameCircle>
-        <GameCircle id={9}>
-
-        </GameCircle>
-        <GameCircle id={10}>
-
-        </GameCircle>
-        <GameCircle id={11}>
-
-        </GameCircle>
-        <GameCircle id={12}>
-
-        </GameCircle>
-        <GameCircle id={13}>
-
-        </GameCircle>
-        <GameCircle id={14}>
-
-        </GameCircle>
-        <GameCircle id={15}>
-
-        </GameCircle>
-        <GameCircle id={16}>
-
-        </GameCircle>
+      {renderCircle(0)}
+      {renderCircle(1)}
+      {renderCircle(2)}
+      {renderCircle(3)}
+      {renderCircle(4)}
+      {renderCircle(5)}
+      {renderCircle(6)}
+      {renderCircle(7)}
+      {renderCircle(8)}
     </div>
   )
 }
